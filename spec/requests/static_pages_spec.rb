375d76d71c2237ac48d11bb8743105e4b28128b2
Bob Creigh <bcreigh@bobcreigh.com>
2013-11-26 22:24:19 -0500

    Static Page Stuff

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
   describe "Home Page" do
     it "Should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +10,7 @@ describe "StaticPages" do
     
     it "should have the title 'Home" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(" | Home")
     end
   end
   
@@ -22,7 +23,7 @@ describe "StaticPages" do
     
     it "should have the title 'Help" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(" | Help")
     end
   end
   
@@ -35,7 +36,20 @@ describe "StaticPages" do
     
     it "should have the title 'About Us" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(" | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title 'About Us" do
+      visit '/static_pages/about'
+      expect(page).to have_title(" | About Us")
     end
   end
 end
